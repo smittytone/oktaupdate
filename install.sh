80b1297f464086017ccc3eb1a9adee252742b152
Tony Smith <smittytone@icloud.com>
2020-06-25 13:26:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+# @version 1.0.1
+
 # Create scrip[ts folder if it's not present
 echo "Installing files..."
-target=~/Library/'Application Scripts'/com.bps.oktaupdate
+target="$HOME/Library/Application Scripts/com.bps.oktaupdate"
 if ! [ -e "$target" ]; then
     mkdir "$target" || exit
 fi
 
 # Install the files
-cp oktaupdate.scpt ~/Library/'Application Scripts'/com.bps.oktaupdate/oktaupdate.scpt
-cp com.bps.oktaupdate.plist ~/Library/LaunchAgents/com.bps.oktaupdate.plist
+cp oktaupdate.scpt "$target/oktaupdate.scpt"
+cp com.bps.oktaupdate.plist "$HOME/Library/LaunchAgents/com.bps.oktaupdate.plist"
 
 # Is the launch agent already installed?
 launchctl list com.bps.oktaupdate >> /dev/null
@@ -17,9 +19,9 @@ status=$?
 if [ $status -eq 0 ]; then
     # Unload the existing version of the launch agent
     echo "Unloading old launch agent..."
-    launchctl unload ~/Library/LaunchAgents/com.bps.oktaupdate.plist
+    launchctl unload "$HOME/Library/LaunchAgents/com.bps.oktaupdate.plist"
 fi
 
 # Load the launch agent
 echo "Loading launch agent..."
-launchctl load ~/Library/LaunchAgents/com.bps.oktaupdate.plist
+launchctl load "$HOME/Library/LaunchAgents/com.bps.oktaupdate.plist"
